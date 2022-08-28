a3b0f28908f15df67709fd2fef570edef719454e
crissdot <crissdotdev@gmail.com>
2022-08-28 18:01:54 -0500

    add polls index template

--- preguntaapp/polls/views.py
+++ preguntaapp/polls/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-  return HttpResponse('Estás en la página principal')
+  latest_question_list = Question.objects.all()
+  context = {
+    'latest_question_list': latest_question_list
+  }
+  return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
   return HttpResponse(f'Estás viendo la pregunta número {question_id}')
