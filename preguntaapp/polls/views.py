3aff656c55a575bd1807050c607f2584d64577bb
crissdot <crissdotdev@gmail.com>
2022-08-29 16:37:08 -0500

    add results template and view

--- preguntaapp/polls/views.py
+++ preguntaapp/polls/views.py
@@ -18,9 +18,6 @@ def detail(request, question_id):
   }
   return render(request, 'polls/detail.html', context)
 
-def results(request, question_id):
-  return HttpResponse(f'Estás viendo los resultados de la pregunta número {question_id}')
-
 def vote(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
 
@@ -36,3 +33,10 @@ def vote(request, question_id):
     selected_choice.votes += 1
     selected_choice.save()
     return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def results(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  context = {
+    'question': question,
+  }
+  return render(request, 'polls/results.html', context)
