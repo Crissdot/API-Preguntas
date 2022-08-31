f4d2f601cf05e1c4e4e0321a88935df00df4caca
crissdot <crissdotdev@gmail.com>
2022-08-30 20:34:14 -0500

    add more test for was_published_recently funtion

--- preguntaapp/polls/tests.py
+++ preguntaapp/polls/tests.py
@@ -7,10 +7,23 @@ from .models import Question
 
 class QuestionModelTest(TestCase):
 
+  def setUp(self):
+    self.question = Question(question_text='test_was_published_recently_with_future_questions')
+
   def test_was_published_recently_with_future_questions(self):
     """was_published_recently returns False for questions whose pub_date is in the future"""
-
     time = timezone.now() + datetime.timedelta(days=30)
-    future_question = Question(question_text='test_was_published_recently_with_future_questions', pub_date=time)
+    self.question.pub_date = time
+    self.assertFalse(self.question.was_published_recently())
 
-    self.assertFalse(future_question.was_published_recently())
+  def test_was_published_recently_with_present_questions(self):
+    """was_published_recently returns True for questions whose pub_date is at the moment"""
+    time = timezone.now()
+    self.question.pub_date = time
+    self.assertTrue(self.question.was_published_recently())
+
+  def test_was_published_recently_with_past_questions(self):
+    """was_published_recently returns False for questions whose pub_date is more than 1 week in the past"""
+    time = timezone.now() - datetime.timedelta(weeks=1, minutes=1)
+    self.question.pub_date = time
+    self.assertFalse(self.question.was_published_recently())
