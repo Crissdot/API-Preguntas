bcdee6fad182b7826267ae1f8aa3db1e075a81fe
crissdot <crissdotdev@gmail.com>
2022-09-08 19:25:45 -0500

    add detail view tests

--- preguntaapp/polls/tests.py
+++ preguntaapp/polls/tests.py
@@ -62,3 +62,17 @@ class QuestionIndexViewTests(TestCase):
     response = self.client.get(reverse('polls:index'))
     self.assertEqual(response.status_code, 200)
     self.assertQuerysetEqual(response.context['latest_question_list'], [question])
+
+class QuestionDetailViewTests(TestCase):
+  def test_future_questions(self):
+    """The detail view of a question with a pub_date in the future returns a 404 error"""
+    future_question = create_question('Future question', 30)
+    response = self.client.get(reverse('polls:detail', args=(future_question.id,)))
+    self.assertEqual(response.status_code, 404)
+
+  def test_past_questions(self):
+    """The detail view of a question with a pub_date in the future returns the question"""
+    past_question = create_question('Past question', -30)
+    response = self.client.get(reverse('polls:detail', args=(past_question.id,)))
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, past_question.question_text)
